024f5946f56867a437b6e17d67d27fa64b9365ef
manaki <mana23happy@hotmail.co.jp>
2020-05-06 03:01:57 +0900

    :+1:天気ページの作成

---
+++ pages/weather/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="p-weather">
+    <div class="c-container"></div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+d80ae072518e92b83d15080e5354ce8b
+http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=d80ae072518e92b83d15080e5354ce8b
+api.openweathermap.org/data/2.5/weather?q=tokyo&appid=7261a693ac98c433aad91f388906a7ec
